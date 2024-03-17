aac7961c22a96135226ec8752fafd1a8e6af7426
ljarillo <pgd_online@hotmail.com>
2024-03-17 15:23:56 -0300

    Implementação da camada de serviço para interagir com a API externa e processar os dados

--- app/Http/Controllers/CepController.php
+++ app/Http/Controllers/CepController.php
@@ -3,30 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Services\CepService;
-use Illuminate\Support\Facades\Http;
 
 class CepController extends Controller
 {
+    protected $cepService;
+
+    public function __construct(CepService $cepService)
+    {
+        $this->cepService = $cepService;
+    }
+
     public function getAddress($cep)
     {
         try {
-            // Faz a requisição para a API do ViaCEP
-            $response = Http::get("https://viacep.com.br/ws/$cep/json/");
-
-            // Verifica se a requisição foi bem-sucedida
-            if ($response->ok()) {
-                $data = $response->json();
-                return response()->json([
-                    'cep' => $data['cep'] ?? null,
-                    'logradouro' => $data['logradouro'] ?? null,
-                    'complemento' => $data['complemento'] ?? null,
-                    'bairro' => $data['bairro'] ?? null,
-                    'localidade' => $data['localidade'] ?? null,
-                    'uf' => $data['uf'] ?? null
-                ]);
-            } else {
-                return response()->json(['error' => 'CEP não encontrado'], 404);
-            }
+            return $this->cepService->getAddressByCep($cep);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Ocorreu um erro ao processar a requisição'], 500);
         }
---
+++ app/Services/CepService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class CepService
+{
+    public function getAddressByCep($cep)
+    {
+        // Faz a requisição para a API do ViaCEP
+        $response = Http::get("https://viacep.com.br/ws/$cep/json/");
+
+        // Verifica se a requisição foi bem-sucedida
+        if ($response->ok()) {
+            $data = $response->json();
+            return response()->json([
+                'cep' => $data['cep'] ?? null,
+                'logradouro' => $data['logradouro'] ?? null,
+                'complemento' => $data['complemento'] ?? null,
+                'bairro' => $data['bairro'] ?? null,
+                'localidade' => $data['localidade'] ?? null,
+                'uf' => $data['uf'] ?? null
+            ]);
+        } else {
+            return response()->json(['error' => 'CEP não encontrado'], 404);
+        }
+    }
+}
